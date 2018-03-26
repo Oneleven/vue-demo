694462fe316913d56969ef3821be762e489abedc
daodao-windows <515144382@qq.com>
2018-03-26 15:23:24 +0800

    vue时间小demo

---
+++ 时钟/main.js
@@ -0,0 +1,49 @@
+function padData(value){
+    return value<10?'0'+value:value  //0不加引号会以数字处理
+}
+
+new Vue({
+    el:'#clock',
+    data:{
+        time:new Date()
+    },
+
+    filters:{
+        formatDate(value){  //这里的value就是{{time|formatDate}}里面的time
+            const data = new Date(value)
+            const month = padData(data.getMonth() + 1) //getMonth返回的是0~11，所以要加上1
+            const year = data.getFullYear()
+            const day = data.getDate()  //区分data.getDay(),表示星期中的第几天0~6
+            const hour = data.getHours()
+            const minute = data.getMinutes()
+            const second = data.getSeconds()
+            return `${year}-${month}-${padData(day)}-${padData(hour)}：${padData(minute)}：${padData(second)}` //模板字符串，${}里面可以使用方法调用
+        }
+    },
+
+    mounted:function(){
+        var _this = this
+        this.timer = setInterval(function(){ //这里的this指向window，所以要在前面绑定_this
+            _this.time = new Date()
+        },1000)
+    },
+
+    beforeDestory:function(){
+        if(this.timer){
+            clearInterval(this.timer)
+        }
+    }
+
+    //es6语法
+   /*  mounted(){
+        this.setIntervaltimer = setInterval(()=>{
+            this.time = new Date()
+        },1000)
+    },
+    beforeDestory(){
+        if(this.timer){
+            clearInterval(this.timer)
+        }
+    } */
+
+})
